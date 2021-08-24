b8f247873623d9e59a535ceb281acf1409776e65
jaimedaniels94 <jaimedaniels94@gmail.com>
2021-08-23 19:06:47 -0500

    Added template and bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'title': 'Jaime daniels',
+        'date_posted': 'August 21, 2021',
+        'content': 'some information'
+    },
+    {
+        'title': 'Jaime daniels',
+        'date_posted': 'August 21, 2021',
+        'content': 'some information'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<p>HOME PAGE!</p>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<p>ABOUT PAGE!</p>"
+    return render_template('about.html', title='about')
 
 
 if __name__ == '__main__':
